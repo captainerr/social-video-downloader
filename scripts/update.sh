2b916837d9afdd2cfedaa35ff54e57d6cc1a05fc
captainerr <ericlanzroberts@gmail.com>
2026-02-10 20:08:22 -0700

    Update update.sh to explicitly use python3 for pip commands in virtual environment setup

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,10 +10,10 @@ cd backend
 if [ ! -f .venv/bin/python3 ]; then
   echo "Creating venv..."
   python3 -m venv .venv
-  .venv/bin/pip install -q --upgrade pip
-  .venv/bin/pip install -q -r requirements.txt
+  .venv/bin/python3 -m pip install -q --upgrade pip
+  .venv/bin/python3 -m pip install -q -r requirements.txt
 else
-  .venv/bin/pip install -q -r requirements.txt
+  .venv/bin/python3 -m pip install -q -r requirements.txt
 fi
 echo "Restarting svd..."
 systemctl restart svd
